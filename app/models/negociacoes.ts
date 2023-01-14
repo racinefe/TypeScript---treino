15a5ab517349dd8897a50da30af31f93573a33a2
Racine Fellipe <racine.fell@gmail.com>
2023-01-13 23:39:46 -0300

    aplicando metodos para deixando o códigos mais limpo

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,12 +1,12 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao){
         this.negociacoes.push(negociacao);
     }
-    lista(): ReadonlyArray<Negociacao>{
+    lista(): readonly Negociacao[]{
         return this.negociacoes;
     }
 }
