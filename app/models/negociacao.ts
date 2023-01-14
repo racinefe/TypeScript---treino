f38ccce2b319f747b5d69282673b9fc878ef6571
Racine Fellipe <racine.fell@gmail.com>
2023-01-14 00:05:44 -0300

    aplicando metodo defensivo

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -6,7 +6,8 @@ export class Negociacao {
     ){}
 
     get data():Date {
-        return this._data;
+        const data = new Date(this._data.getTime());
+        return data;
     }
 
     get quantidade():number {
